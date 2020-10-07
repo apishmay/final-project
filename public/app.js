fd40434a6a932366bf7646c425360cc0ad36aa9a
apishmay <apishmay@gmail.com>
2020-10-07 13:43:16 +0800

    10/7更改

--- public/app.js
+++ public/app.js
@@ -1,19 +1,19 @@
-const studentsTable = document.querySelector('#stu-table');
-const form = document.querySelector("#add-students-form");
+const cafeTable = document.querySelector('#cafe-table');
+const form = document.querySelector("#add-cafe-form");
 // create element & render 
-function renderStudents(doc){
+function rendercofe(doc) {
     let td1 = document.createElement("td");
     let td2 = document.createElement("td");
     let td3 = document.createElement("td");
     let tr = document.createElement("tr");
     tr.setAttribute('data-id', doc.id);
     td1.textContent = doc.data().name;
-    td2.textContent = doc.data().age;
-    td3.textContent = doc.data().gender;
+    td2.textContent = doc.data().money;
+    td3.textContent = doc.data().hot;
     tr.appendChild(td1);
     tr.appendChild(td2);
     tr.appendChild(td3);
-    
+
     // delete 
     let cross = document.createElement('div');
     cross.textContent = 'x';
@@ -22,17 +22,17 @@ function renderStudents(doc){
         test.stopPropagation();
         let id = test.target.parentElement.getAttribute('data-id');
         console.log(id);
-        db.collection('ClassA').doc(id).delete();
+        db.collection('Cafe').doc(id).delete();
     });
     //
 
-    studentsTable.appendChild(tr);
+    cafeTable.appendChild(tr);
 }
 
 // getting data 
-db.collection('ClassA').get().then(data => {
+db.collection('Cafe').get().then(data => {
     data.docs.forEach(doc => {
-        renderStudents(doc);
+        rendercofe(doc);
     });
 });
 // 
@@ -40,13 +40,13 @@ db.collection('ClassA').get().then(data => {
 // add data
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    db.collection('ClassA').add({
+    db.collection('Cafe').add({
         name: form.name.value,
-        gender: form.gender.value,
-        age: form.age.value
+        money: form.money.value,
+        hot: form.hot.value
     });
     form.name.value = '';
-    form.gender.value = '';
-    form.age.value = '';
+    form.money.value = '';
+    form.hot.value = '';
 });
 //
--- routers/Yala.js
+++ routers/Yala.js
@@ -8,13 +8,16 @@ router.use(function timeLog(req, res, next) {
 });
 // define the home page route
 
-router.get('/', function(req, res) {
-  res.send('Bob\'s home page');
+router.get('/', function (req, res) {
+  res.send('亞拉Yala');
 });
 
 // define the about route
-router.get('/about', function(req, res) {
-  res.send('About Bob');
+router.get('/about', function (req, res) {
+  res.send('關於亞拉Yala</br>' +
+    '\n <img src="https://tw.appledaily.com/resizer/N7iyl-parNCuIA76K8ywcKFAAJc=/427x640/filters:quality(100)/cloudfront-ap-northeast-1.images.arcpublishing.com/appledaily/BKA4RVYRRFHR2SMOHTN4NWQU4E.jpg" height="60%">'
+  );
+
 });
 
 module.exports = router;
